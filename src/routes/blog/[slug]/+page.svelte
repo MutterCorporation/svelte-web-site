bf902da7796cb296de2cc4d8f85d1a26a7de08c4
Maikon Weber <maikonweber@gmail.com>
2024-08-30 13:20:05 -0300

    add svelte

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -67,9 +67,7 @@
 	<div class="container">
 		<div class="post">
 			<img src={img} alt="Post Image" class="post-image" />
-			<div class="post-body" innerHTML={previewHtml}>
-				{@html previewHtml}
-			</div>
+			<div class="post-body" innerHTML={previewHtml}></div>
 		</div>
 	</div>
 {/if}
