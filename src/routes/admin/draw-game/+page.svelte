057a2682ca3a5ee0e040bd283ca8b3d2a9ed5d68
Maikon Weber <maikonweber@gmail.com>
2024-08-30 21:39:15 -0300

    Add canvas

---
+++ src/routes/admin/draw-game/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let canvas;
+    let context;
+  
+    // Função para desenhar no canvas
+    function draw() {
+      if (context) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+        context.arc(canvas.width / 2, canvas.height / 2, 50, 0, 2 * Math.PI);
+        context.fillStyle = 'blue';
+        context.fill();
+        context.stroke();
+      }
+    }
+  
+    // Configura o canvas e o contexto quando o componente é montado
+    onMount(() => {
+      context = canvas.getContext('2d');
+      draw();
+    });
+  </script>
+  
+  <canvas bind:this={canvas} width="400" height="400" style="border: 1px solid black;"></canvas>
+  
